f16aa99eaf0299ddaab53d9185ee8f10fae35932
Christine <tinarosemoraa@gmail.com>
2025-07-08 21:32:38 +0300

    Remove 5-to_interger.js from repository

--- 5-to_interger.js
+++
@@ -1,8 +0,0 @@
-const arg = process.argv[2];
-const number = Number(arg);
-
-if (!isNaN(number) && Number.isInteger(number)) {
-  console.log(`My number: ${number}`);
-} else {
-  console.log('Not a number');
-}
